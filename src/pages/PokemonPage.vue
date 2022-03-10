78806036397a8dc9ccdbe1e0fdc5282178064957
Ivan Jaramillo <ivan@aiwifi.io>
2022-03-09 22:43:08 -0600

    Add components structure and styles

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Quien es este pokemon? </h1>
+    <PokemonPicture />
+    <PokemonOptions />
+  </div>
+</template>
+
+<script>
+import PokemonOptions from "@/components/PokemonOptions";
+import PokemonPicture from "@/components/PokemonPicture";
+
+export default {
+  components:{
+    PokemonPicture,
+    PokemonOptions
+  }
+}
+</script>
