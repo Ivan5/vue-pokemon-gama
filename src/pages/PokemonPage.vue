b75cf3bb257e969b12c8f4e338667d907c27dbf4
Ivan Jaramillo <ivan@aiwifi.io>
2022-03-15 20:53:46 -0600

    Make get pokemons names and show it

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,19 +1,33 @@
 <template>
   <div>
     <h1>Quien es este pokemon? </h1>
-    <PokemonPicture />
-    <PokemonOptions />
+    <PokemonPicture :pokemonId="48" :showPokemon="false"/>
+    <PokemonOptions :pokemons="pokemonArr"/>
   </div>
 </template>
 
 <script>
 import PokemonOptions from "@/components/PokemonOptions";
 import PokemonPicture from "@/components/PokemonPicture";
+import getPokemonsOptions from'@/helpers/getPokemonOptions';
 
 export default {
   components:{
     PokemonPicture,
     PokemonOptions
+  },
+  data(){
+    return {
+      pokemonArr: []
+    }
+  },
+  methods:{
+    async mixPokemonArray() {
+      this.pokemonArr = await getPokemonsOptions()
+    }
+  },
+  mounted() {
+    this.mixPokemonArray()
   }
 }
 </script>
